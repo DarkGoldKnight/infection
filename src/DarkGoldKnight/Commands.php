e37de5016a35b36ab6718e73ce6204910363d89e
DarkGoldKnight <30422975+DarkGoldKnight@users.noreply.github.com>
2017-08-01 12:06:02 +1200

    Update Commands.php

--- src/DarkGoldKnight/Commands.php
+++ src/DarkGoldKnight/Commands.php
@@ -1,1 +1,30 @@
-
+<?php
+namespace DarkGoldKnight\infection;
+use pocketmine\permission\Permission;
+use pocketmine\command\Command;
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+use DarkGoldKnight\infection\Main;
+class Commands implements CommandExecutor {
+    public function __construct(Main $plugin){
+        $this->plugin = $plugin;
+    }
+    public function onCommand(CommandSender $sender, Command $cmd, $label, array $args) {
+        $cmd = strtolower($cmd->getName());
+        switch($cmd){
+            case "infect":
+                if($sender instanceof Player){
+                    $this->plugin->setInfected($sender);
+                    $sender->sendTip(Main::COLOR("&eDebug!"));
+                }
+                break;
+            case "gi":
+                $sender->sendMessage("in:" . $this->plugin->getInfected());
+                break;
+        }
+    }
+}
